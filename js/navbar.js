e35c56ca4580c8d6169c62a7f88f30f5964bc3c2
carriesevents <65443731+carriesevents@users.noreply.github.com>
2024-12-19 09:41:44 +0000

    update

--- js/navbar.js
+++ js/navbar.js
@@ -3,20 +3,12 @@ const navbarHtml = `
   <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
       <a class="navbar-brand" href="index.html">
-        <img src="logo.png" alt="Carrie's Events">
+        <img src="./images/logo.PNG" alt="Carrie's Events">
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-        <span></span>
-        <span></span>
-        <span></span>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <div class="mobile-nav-header">
-          <img src="logo.png" alt="Carrie's Events" class="mobile-logo">
-          <button class="close-nav" data-toggle="collapse" data-target="#navbarNav">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a class="nav-link" href="index.html">Home</a>
@@ -25,10 +17,10 @@ const navbarHtml = `
             <a class="nav-link" href="about.html">About</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="productsDropdown" role="button" data-toggle="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="productsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Products
             </a>
-            <div class="dropdown-menu">
+            <div class="dropdown-menu" aria-labelledby="productsDropdown">
               <a class="dropdown-item" href="candycart.html">Candy Cart</a>
               <a class="dropdown-item" href="smallcandycart.html">Small Candy Cart</a>
               <a class="dropdown-item" href="donutwall.html">Donut Wall</a>
@@ -47,16 +39,6 @@ const navbarHtml = `
             <a class="nav-link" href="contact.html">Contact</a>
           </li>
         </ul>
-        <div class="mobile-nav-footer">
-          <div class="social-links">
-            <a href="https://www.facebook.com/carrie.harkiss/" target="_blank">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://www.instagram.com/carrieharkiss" target="_blank">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   </nav>
@@ -85,55 +67,5 @@ document.addEventListener('DOMContentLoaded', function() {
       item.classList.add('active');
       item.closest('.nav-item').querySelector('.nav-link').classList.add('active');
     }
-  });
-});
-
-// Handle mobile navigation
-document.addEventListener('DOMContentLoaded', function() {
-  const navbar = document.querySelector('.navbar');
-  const navbarCollapse = document.querySelector('.navbar-collapse');
-  const toggler = document.querySelector('.navbar-toggler');
-  const closeNav = document.querySelector('.close-nav');
-  
-  // Add scroll effect
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
-  
-  // Handle mobile menu open/close
-  toggler.addEventListener('click', function() {
-    document.body.classList.toggle('nav-open');
-    navbarCollapse.classList.toggle('show');
-  });
-  
-  closeNav.addEventListener('click', function() {
-    document.body.classList.remove('nav-open');
-    navbarCollapse.classList.remove('show');
-  });
-  
-  // Close mobile menu when clicking outside
-  document.addEventListener('click', function(e) {
-    if (document.body.classList.contains('nav-open') && 
-        !e.target.closest('.navbar-collapse') && 
-        !e.target.closest('.navbar-toggler')) {
-      document.body.classList.remove('nav-open');
-      navbarCollapse.classList.remove('show');
-    }
-  });
-  
-  // Handle dropdown on mobile
-  const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-  dropdownToggles.forEach(toggle => {
-    toggle.addEventListener('click', function(e) {
-      if (window.innerWidth < 992) {
-        e.preventDefault();
-        const dropdownMenu = this.nextElementSibling;
-        dropdownMenu.classList.toggle('show');
-      }
-    });
   });
 }); 
